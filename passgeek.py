12b01c72014c1369ccd9f7589df08e0add1a9c7d
TheSwagVader <egorzverev666@gmail.com>
2020-08-02 18:01:44 +0300

    add some functional

--- passgeek.py
+++ passgeek.py
@@ -16,15 +16,41 @@ while True:
         print('2. Generate with SHA256')
         print('3. Generate with SHA512')
         secondChoice = input('>')
+        usingShake = True if input('Using shake?(y/n)') == 'y' else False
+        usingLoops = True if input('Using loops?(y/n)') == 'y' else False
+        loops = 0
+        if usingLoops:
+            loops = int(input('Enter loops value: '))
         if secondChoice == '1':
             seed = input('Enter password seed: ')
-            password.generateMD5(seed)
+            if usingLoops:
+                password.loopGenMD5(seed, loops)
+            elif usingShake:
+                password.generateShakeMD5(seed)
+            elif usingLoops and usingShake:
+                password.loopGenShakeMD5(seed, loops)
+            else:
+                password.generateMD5(seed)
         if secondChoice == '2':
             seed = input('Enter password seed: ')
-            password.generateSHA256(seed)
+            if usingLoops:
+                password.loopGenSHA256(seed, loops)
+            elif usingShake:
+                password.generateShakeSHA256(seed)
+            elif usingLoops and usingShake:
+                password.loopGenShakeSHA256(seed, loops)
+            else:
+                password.generateSHA256(seed)
         if secondChoice == '3':
             seed = input('Enter password seed: ')
-            password.generateSHA512(seed)
+            if usingLoops:
+                password.loopGenSHA512(seed, loops)
+            elif usingShake:
+                password.generateShakeSHA512(seed)
+            elif usingLoops and usingShake:
+                password.loopGenShakeSHA512(seed, loops)
+            else:
+                password.generateSHA512(seed)
         print(f'Generated password: {password.getPassword()}')
     elif chioce == 'x':
         break
